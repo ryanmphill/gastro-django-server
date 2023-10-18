09680a32db32538f107fa1d6f6db7c83b2735f3a
Ryan Phillips <ryanmphill@gmail.com>
2023-10-18 18:05:42 -0500

    Added new fixtures to seed database and added new gastro_user model

--- gastrognome_api/models/__init__.py
+++ gastrognome_api/models/__init__.py
@@ -6,3 +6,4 @@ from .ingredient import Ingredient
 from .recipe_category import RecipeCategory
 from .recipe_ingredient import RecipeIngredient
 from .recipe import Recipe
+from .gastro_user import GastroUser
--- seed_data.sh
+++ seed_data.sh
@@ -7,3 +7,12 @@ python3 manage.py makemigrations gastrognome_api
 python3 manage.py migrate gastrognome_api
 python3 manage.py loaddata users
 python3 manage.py loaddata tokens
+python3 manage.py loaddata gastro_users
+python3 manage.py loaddata categories
+python3 manage.py loaddata ingredients
+python3 manage.py loaddata genres
+python3 manage.py loaddata recipes
+python3 manage.py loaddata recipe_categories
+python3 manage.py loaddata recipe_ingredients
+python3 manage.py loaddata favorites
+python3 manage.py loaddata follows
